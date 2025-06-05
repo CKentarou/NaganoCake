d317cf6cda6b3759437f7e5b62a1b1e6e319ab27
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-05 09:34:18 +0900

    新しいジャンルテーブルを作成し、関連するマイグレーションファイルとモデルを追加。スキーマファイルを更新し、ジャンルのフィクスチャを追加。バイナリデータベースファイルも更新。

--- service/db/schema.rb
+++ service/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_05_002952) do
+ActiveRecord::Schema.define(version: 2025_06_05_003351) do
 
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2025_06_05_002952) do
     t.boolean "membership_status", default: true, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "addresses", "customers"
