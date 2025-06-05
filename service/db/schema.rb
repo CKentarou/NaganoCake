91dfd2e66aadabd7103f9b591fc1bbe639faf97d
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-05 09:31:45 +0900

    新しい住所テーブルを作成し、関連するマイグレーションファイルとモデルを追加。スキーマファイルを更新し、アドレスのフィクスチャを追加。バイナリデータベースファイルも更新。

--- service/db/schema.rb
+++ service/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_05_002227) do
+ActiveRecord::Schema.define(version: 2025_06_05_002952) do
+
+  create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "recipient_name", null: false
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
+  end
 
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -44,4 +54,5 @@ ActiveRecord::Schema.define(version: 2025_06_05_002227) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "customers"
 end
