14c6c6dd73713333b847e2d99b12095e0f2d013c
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-05 09:52:59 +0900

    カートアイテムモデルを追加し、関連するマイグレーションファイルとスキーマを更新。カートアイテムのフィクスチャとテストファイルも作成。

--- service/db/schema.rb
+++ service/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_05_004532) do
+ActiveRecord::Schema.define(version: 2025_06_05_005206) do
 
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 2025_06_05_004532) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.bigint "customer_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_cart_items_on_customer_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
   end
 
   create_table "customers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -72,5 +82,7 @@ ActiveRecord::Schema.define(version: 2025_06_05_004532) do
   end
 
   add_foreign_key "addresses", "customers"
+  add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "items"
   add_foreign_key "items", "genres"
 end
