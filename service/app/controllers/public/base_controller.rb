48e90267c3c40bba75e96e1e1ef3801956463647
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-10 11:07:17 +0900

    アプリケーションコントローラーからサインインおよびサインアウト後のリダイレクトパスを削除し、管理者および一般ユーザー用のベースコントローラーにそれぞれのリダイレクトパスを追加。

--- service/app/controllers/public/base_controller.rb
+++ service/app/controllers/public/base_controller.rb
@@ -1,3 +1,11 @@
 class Public::BaseController < ApplicationController
   before_action :authenticate_customer!, except: [:top, :about]
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
