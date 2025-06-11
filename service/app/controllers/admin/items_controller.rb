dd864c7576f72bc1e8847c64c6c6622669fe316c
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-11 10:34:05 +0900

    商品一覧ページのレイアウトを改善し、商品情報をテーブル形式で表示するように変更。ジャンル情報を事前に読み込むためにincludesを使用し、N+1問題に対処。

--- service/app/controllers/admin/items_controller.rb
+++ service/app/controllers/admin/items_controller.rb
@@ -1,11 +1,14 @@
 class Admin::ItemsController < Admin::BaseController
   def index
+    # includesを使用してgenreを事前に読み込む(N+1問題対策)
+    @items = Item.includes(:genre).all
   end
 
   def show
   end
 
   def new
+    
   end
 
   def edit
