44ff77491cbad1f3b19377f799c35cfeb7ea0cbb
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-16 09:00:36 +0000

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  # 顧客用
-  # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
+    registrations: 'public/registrations',
     sessions: 'public/sessions'
   }
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
   }
 
   get 'about' => 'public/homes#about'
