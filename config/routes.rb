545302ba6ae2f69b32d7fbcb3f5b8d22a63635ef
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-16 08:07:58 +0000

    全コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,28 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  get 'about' => 'public/homes#about'
+  scope module: :public do
+    root to: 'homes#top'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/thanks' => 'orders#thanks'
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :show, :new, :edit, :create, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show]
+    resources :order_details, only: [:update]
+  end
 end
