c84f177d41b96e02bbdd07476843cbfad06aa70b
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-23 10:06:29 +0000

    注文詳細と注文情報の更新機能を追加し、注文詳細ビューを整備しました。注文情報の表示を改善し、製作ステータスの更新フォームを実装。

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,18 @@
 class Admin::OrdersController < Admin::BaseController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.includes(:item)
   end
 
   def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    redirect_to admin_order_path(order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
