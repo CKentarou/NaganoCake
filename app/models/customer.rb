086af5e3105c0ee8eb95c1e420ac3267e2666c00
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-23 01:48:34 +0000

    顧客モデルに退会済みのアカウントを考慮した認証メソッドを追加し、アプリケーションレイアウトに成功メッセージとエラーメッセージの表示機能を実装。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,13 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  # 有効な会員のみログイン可能にする
+  def active_for_authentication?
+    super && membership_status?
+  end
+
+  def inactive_message
+    membership_status? ? super : "このアカウントは退会済みです。"
+  end
 end
