5bbbb236c032df48ff47719a2fcb04de58953393
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-23 10:01:59 +0000

    注文リソースに更新アクションを追加し、注文情報の更新を可能にしました。

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show, :new, :edit, :create, :update]
     resources :genres, only: [:index, :edit, :create, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show]
+    resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
 end
