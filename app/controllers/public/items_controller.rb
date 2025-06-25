fee343c2a454aecafbd39810fc9fc4fd2392dfd4
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-25 12:53:09 +0000

    商品一覧にジャンルフィルタ機能を追加し、ジャンル名をリンクとして表示するようにしました。また、選択されたジャンルに応じてタイトルを変更する機能を実装しました。

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,8 +2,13 @@ class Public::ItemsController < Public::BaseController
   skip_before_action :authenticate_customer!, only: [:index, :show]
 
   def index
-    @items = Item.page(params[:page]).per(8)
+    if params[:genre_id].present?
+      @items = Item.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
     @genres = Genre.all
+    @genre = Genre.find(params[:genre_id]) if params[:genre_id].present?
   end
 
   def show
