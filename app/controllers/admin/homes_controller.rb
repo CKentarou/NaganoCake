6e479559975ee2d1f118573824b8faae77366ece
CKentarou <k023c0018@g.neec.ac.jp>
2025-06-26 01:22:28 +0000

    注文履歴の表示を顧客IDに基づいてフィルタリングする機能を追加しました。また、顧客の注文履歴へのリンクを修正し、適切なパスにリダイレクトするようにしました。

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,9 @@
 class Admin::HomesController < Admin::BaseController
   def top
-    @orders = Order.page(params[:page]).per(10)
+    if params[:customer_id].present?
+      @orders = Order.where(customer_id: params[:customer_id]).page(params[:page]).per(10)
+    else
+      @orders = Order.page(params[:page]).per(10)
+    end
   end
 end
